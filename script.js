f602f0ce756d3e2d2e8d3a0cbf48b885f9831516
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-07-12 12:46:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) {
+    return -Infinity;
+  }
+
+  // Remove duplicate elements from the array
+  const uniqueArr = Array.from(new Set(arr));
+
+  // If all elements are the same, return -Infinity
+  if (uniqueArr.length === 1) {
+    return -Infinity;
+  }
+
+  // Sort the array in descending order
+  const sortedArr = uniqueArr.sort((a, b) => b - a);
+
+  // Return the second highest element
+  return sortedArr[1];
+}
+
